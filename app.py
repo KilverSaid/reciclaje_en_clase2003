fb1ada1f65bad2ba6659e7ed7cd9ee78dcc9d1e5
KilverSaid <140364862+KilverSaid@users.noreply.github.com>
2026-06-23 10:28:51 -0600

    Fix syntax error in set_page_config call

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ import streamlit as st
 import tensorflow as f
 from PIL import image
 
-st.set_page_config(page_title= "Reciclaje IA-ISC".layout="centered")
+st.set_page_config(page_title= "Reciclaje IA-ISC", layout="centered")
 st.title("Modelo preictivo Reciclaje clase de IA-ISC-Campus Comayagua 2026")
 st.write("Suba una imagen para clasificar con el modelo MobileNetV2 pre-entrenada")
