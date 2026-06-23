8c54a4d2fe9078f417385f3b542d423e57b9de5b
KilverSaid <140364862+KilverSaid@users.noreply.github.com>
2026-06-23 10:28:20 -0600

    Add initial setup for image classification app

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+import json
+from pathlib import Path
+import numpy as np
+import streamlit as st
+import tensorflow as f
+from PIL import image
 
+st.set_page_config(page_title= "Reciclaje IA-ISC".layout="centered")
+st.title("Modelo preictivo Reciclaje clase de IA-ISC-Campus Comayagua 2026")
+st.write("Suba una imagen para clasificar con el modelo MobileNetV2 pre-entrenada")
