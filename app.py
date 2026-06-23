a363cb3f6c1e89de0a506fe530f40f4e14dc4138
KilverSaid <140364862+KilverSaid@users.noreply.github.com>
2026-06-23 10:30:49 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import json
 from pathlib import Path
 import numpy as np
 import streamlit as st
-import tensorflow as f
-from PIL import image
+import tensorflow as tf 
+from PIL import Image    
 
-st.set_page_config(page_title= "Reciclaje IA-ISC", layout="centered")
-st.title("Modelo preictivo Reciclaje clase de IA-ISC-Campus Comayagua 2026")
+st.set_page_config(page_title="Reciclaje IA-ISC", layout="centered")
+st.title("Modelo predictivo Reciclaje clase de IA-ISC-Campus Comayagua 2026") 
 st.write("Suba una imagen para clasificar con el modelo MobileNetV2 pre-entrenada")
