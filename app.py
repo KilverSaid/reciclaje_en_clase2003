18ade0f1d6fa504ecfc5697dc9db4731b2d96e85
KilverSaid <140364862+KilverSaid@users.noreply.github.com>
2026-06-23 10:37:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ st.write("Suba una imagen para clasificar con el modelo MobileNetV2 pre-entrenad
 IMG_SIZE=(224,224)
 MODEL_DIR=Path("modelo_reciclaje_mobilenet")
 CLASS_PATH=MODEL_DIR/"class_names.json"
-MODEL-PATHS=[MODEL_DIR]
+MODEL-PATHS=[MODEL_DIR/"waste_mobilenet.keras", MODEL_DIR/]
